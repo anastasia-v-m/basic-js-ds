f881273e9d44f3ba5c93cb36a39dbcbaa9dd2804
anastasia-v-m <matushevskayaanastasia@gmail.com>
2021-10-18 17:31:00 +0300

    fix: BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,39 +8,139 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 module.exports = class BinarySearchTree {
 
+  constructor(nodeValue) {
+    this.rootElement = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootElement;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let node = new TreeNode(data);
+    if (this.rootElement === null) {
+      this.rootElement = node;
+    } else {
+      this.addNewNode(this.rootElement, node);
+    }
+
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addNewNode(rootNode, currentNode) {
+    if (currentNode.data < rootNode.data) {
+      if (rootNode.left === null) {
+        rootNode.left = currentNode;
+      } else {
+          this.addNewNode(rootNode.left, currentNode);
+      }
+    } else {
+      if (rootNode.right === null) {
+        rootNode.right = currentNode;
+      } else {
+          this.addNewNode(rootNode.right, currentNode);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.searchByValue(this.rootElement, data).hasValue;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  searchByValue(currentNode, data) {
+    const result = {
+      node: null,
+      hasValue: false
+    };
+    if (currentNode === null) {
+      result.node = currentNode;
+      result.hasValue = false;
+      return result;
+    } else if (data < currentNode.data) {
+      return this.searchByValue(currentNode.left, data);
+    } else if (data > currentNode.data) {
+      return this.searchByValue(currentNode.right, data);
+    } else {
+      result.node = currentNode;
+      result.hasValue = true;
+      return result;
+    }
+  }
+
+  find(data) {
+    return this.searchByValue(this.rootElement, data).node;
+  }
+
+  remove(data) {
+    this.root = this.removeByValue(this.rootElement, data);  
+  }
+
+  removeByValue(currentNode, data) {
+    if (currentNode === null) {
+      return null;
+    } else if (data < currentNode.data) {
+      currentNode.left = this.removeByValue(currentNode.left, data);
+      return currentNode;
+    } else if (data > currentNode.data) {
+      currentNode.right = this.removeByValue(currentNode.right, data);
+      return currentNode;
+    } else {
+      
+      if (currentNode.left === null && currentNode.right === null) {
+        currentNode = null;
+        return currentNode;
+      }
+      
+      if (currentNode.left === null) {
+        currentNode = currentNode.right;
+        return currentNode;
+      } else if(currentNode.right === null) {
+        currentNode = currentNode.left;
+        return currentNode;
+      }
+      
+      let insteadNode = this.searchMin(currentNode.right);
+      currentNode.data = insteadNode.data;
+      currentNode.right = this.removeByValue(currentNode.right, insteadNode.data);
+      return currentNode;
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.rootElement === null) {
+      return null;
+    }
+    return this.searchMin(this.rootElement).data;
+  }
+
+  searchMin(currentNode) {
+    if (currentNode.left === null) {
+      return currentNode;
+    } else {
+      return this.searchMin(currentNode.left);
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.rootElement === null) {
+      return null;
+    }
+    return this.searchMax(this.rootElement).data;  
   }
 
+  searchMax(currentNode) {
+    if (currentNode.right === null) {
+      return currentNode;
+    } else {
+      return this.searchMax(currentNode.right);
+    }
+  }
+
+}
+
+class TreeNode {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
 }
